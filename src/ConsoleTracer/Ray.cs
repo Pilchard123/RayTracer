62504191155bec5c5c0fe86c1cacb5c25474eb1d
Richard Ward <ward.richard@hotmail.co.uk>
2020-04-30 23:33:10 +0100

    Add Ray class

---
+++ src/ConsoleTracer/Ray.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleTracer
+{
+    readonly struct Ray
+    {
+        public Vector3 Origin { get; }
+        public Vector3 Direction { get; }
+
+
+        public Ray(in Vector3 origin, in Vector3 direction)
+        {
+            Origin = origin;
+            Direction = direction;
+        }
+
+        public Vector3 PointAtParameter(double t) => Origin + (Direction * t);
+    }
+}
