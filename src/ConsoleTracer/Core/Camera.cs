5572795d222b4c64f74dfce59b6e13330df2d9ee
Richard Ward <ward.richard@hotmail.co.uk>
2020-05-02 22:05:45 +0100

    Move camera backwards

--- src/ConsoleTracer/Core/Camera.cs
+++ src/ConsoleTracer/Core/Camera.cs
@@ -6,7 +6,7 @@
         private readonly Vector3 _lowerLeftCorner = new Vector3(-2, -1, -1);
         private readonly Vector3 _horizontal = new Vector3(4, 0, 0);
         private readonly Vector3 _vertical = new Vector3(0, 2, 0);
-        private readonly Vector3 _origin = new Vector3(0, 0, 0);
+        private readonly Vector3 _origin = new Vector3(0, 0, 15);
 
         public Ray GetRay(double u, double v) => new Ray(_origin, _lowerLeftCorner + (u * _horizontal) + (v * _vertical) - _origin);
 
